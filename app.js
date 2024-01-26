4994af3f8a483e810883c3dbf571e9a6f4a19841
EuniceCeja <eceja93@hotmail.com>
2024-01-25 21:18:26 -0600

    Cambios

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí.
+
+function palabraLarga(str, array) {
+    return array.filter(word => word.length > str.length); //filter para iterar entre los elementos del array
+}
+
+const animales = ["Perro", "Gato", "Tigre", "León", "Mapache", "Rana"];
+const palabra = palabraLarga("animal", animales);
+console.log(palabra);
+
